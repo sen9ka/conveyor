796dc1a3ed73f3c0dd6ab53c3281008298e7cce3
sen9ka <ars-shvec@yandex.ru>
2023-01-12 17:16:32 +0300

    Тестирование

--- src/main/java/ru/senya/conveyor/services/ConveyorService.java
+++ src/main/java/ru/senya/conveyor/services/ConveyorService.java
@@ -39,20 +39,20 @@ public class ConveyorService {
     public static final double MIDDLEAGE_FEMALE_BONUS = 0.3;
     public static final double MIDDLEAGE_MALE_BONUS = 0.3;
 
-//    @Value("${baseLoanRate}")
-//    private BigDecimal baseLoanRate;
-////
-    BigDecimal baseLoanRate = new BigDecimal("10");
-////
-//    @Value("${insuranceBonus}")
-//    private BigDecimal insuranceBonus;
-////
-    BigDecimal insuranceBonus = new BigDecimal("0.3");
-//
-//    @Value("${salaryClientBonus}")
-//    private BigDecimal salaryClientBonus;
-////
-    BigDecimal salaryClientBonus = new BigDecimal("0.1");
+    @Value("${baseLoanRate}")
+    private BigDecimal baseLoanRate;
+
+//    BigDecimal baseLoanRate = new BigDecimal("10");
+
+    @Value("${insuranceBonus}")
+    private BigDecimal insuranceBonus;
+
+//    BigDecimal insuranceBonus = new BigDecimal("0.3");
+
+    @Value("${salaryClientBonus}")
+    private BigDecimal salaryClientBonus;
+
+//    BigDecimal salaryClientBonus = new BigDecimal("0.1");
 
     /*
     conveyor/offers
